d74a061c0787e0ff9491ab7b81763ba20c2822ad
Tô Thành Công <tcvn1985@gmail.com>
2016-01-16 10:15:26 +0700

    script cai dat grafana 2.6.x

---
+++ scripts/grafana-srv.sh
@@ -0,0 +1,20 @@
+######################################################################
+### Script cai dat Grafana 2.6.x
+### CongTo tcvn1985@gmail.com
+### 16/01/2016
+######################################################################
+#!/bin/bash
+
+
+echo "deb https://packagecloud.io/grafana/stable/debian/ wheezy main" >> /etc/apt/sources.list
+curl https://packagecloud.io/gpg.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install grafana
+
+echo "Khoi dong Grafana"
+sleep 5
+sudo service grafana-server start
+
+echo "Cho phep grafana khoi dong cung OS"
+sleep 3
+sudo update-rc.d grafana-server defaults 95 10
